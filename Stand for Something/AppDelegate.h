7f48a91e09aa29324ee84907d9e34c62b5834c3b
Alper Çugun <github@alper.nl>
2013-07-19 23:28:03 +0200

    Initial Commit

---
+++ Stand for Something/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Stand for Something
+//
+//  Created by Alper Cugun on 19/7/13.
+//  Copyright (c) 2013 Alper Cugun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
