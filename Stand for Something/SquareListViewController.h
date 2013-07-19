b5dd47d2f945fcab09c9f13145fa0362f7af5ffb
Alper Çugun <github@alper.nl>
2013-07-20 00:04:42 +0200

    added class to table view controller

---
+++ Stand for Something/SquareListViewController.h
@@ -0,0 +1,13 @@
+//
+//  SquareListViewController.h
+//  Stand for Something
+//
+//  Created by Alper Cugun on 20/7/13.
+//  Copyright (c) 2013 Alper Cugun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SquareListViewController : UITableViewController
+
+@end
