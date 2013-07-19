419000c5382ba64342d1f0b77c184627df75a216
Alper Çugun <github@alper.nl>
2013-07-20 00:07:51 +0200

    configure cells

--- Stand for Something/SquareListViewController.m
+++ Stand for Something/SquareListViewController.m
@@ -56,7 +56,7 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
+    static NSString *CellIdentifier = @"SquareCell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
     // Configure the cell...
